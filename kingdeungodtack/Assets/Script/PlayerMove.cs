6f66a1d1d66a7fa2a2013df95247015feba8a5cd
JuYongWoo <72632207+dyddndn@users.noreply.github.com>
2021-07-20 03:14:45 +0900

    SuddenAttack
    SuddenAttack

---
+++ kingdeungodtack/Assets/Script/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 150;
+    CharacterController charCtrl;
+    void Start()
+    {
+        charCtrl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+      
+
+        // 1. 사용자의 입력에 따라
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 2. 앞뒤 좌우로 방향을 만든다.
+        Vector3 dir = Vector3.right * h + Vector3.forward * v;
+
+        //카메라가 보고있는 방향을 앞 방향으로 변경한다.
+        dir = Camera.main.transform.TransformDirection(dir);
+        //로컬스페이스에서 월드스페이스로 변환 해준다. (트렌스폼 기준으로 결과를 바꾼다.)
+        dir = new Vector3(dir.x, 0, dir.z);
+
+        // 3. 그 방향으로 이동한다.
+        // P = P0 + vt
+        //transform.position += dir * speed * Time.deltaTime;
+        charCtrl.SimpleMove(dir * speed * Time.deltaTime);
+    }
+
+
+}
---
+++ kingdeungodtack/Assets/Script/bulletmove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletmove : MonoBehaviour
+{
+    Vector3 forr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        forr = Camera.main.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(forr*20 * Time.deltaTime);
+    }
+
+}
