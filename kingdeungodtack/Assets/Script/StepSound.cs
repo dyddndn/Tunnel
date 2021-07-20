bb9b20869220bfe44fb891ea8c3fa7cf598a2af8
JuYongWoo <72632207+dyddndn@users.noreply.github.com>
2021-07-21 01:38:11 +0900

    sound update
    sound update

---
+++ kingdeungodtack/Assets/Script/StepSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepSound : MonoBehaviour
+{
+    public AudioClip stepsound;
+    float soundtime = 0.0f;
+    bool cansound = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().clip = stepsound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soundtime += Time.deltaTime;
+
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (dir.sqrMagnitude > 0.1f && soundtime > 0.5f)
+        {
+            GetComponent<AudioSource>().Play();
+            cansound = false;
+            soundtime = 0.0f;
+        }
+    }
+}
